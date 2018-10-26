46adade7e5934ffd3a8dfb96d5b3b0f456a2c6fd
KibaePark <book8484@naver.com>
2018-10-26 14:13:06 +0900

    first commit

---
+++ app/src/main/java/com/example/ki/a10_25/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.ki.a10_25;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+public class MainActivity extends AppCompatActivity {
+    DatePicker datePicker;//날짜를 선택하는 달력
+    Button button1;
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        datePicker=(DatePicker)findViewById(R.id.datePicker);
+
+        button1  =(Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class );
+                startActivity(intent);
+            }
+        });
+        button2 =(Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
