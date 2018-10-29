197304e37092d511e07643f2696b66e44a71204e
dayoungyoon <dy199823@naver.com>
2018-10-29 17:58:18 +0900

    유저정보 json

--- app/src/main/java/com/example/ki/a10_25/KakaoLoginActivity.java
+++ app/src/main/java/com/example/ki/a10_25/KakaoLoginActivity.java
@@ -1,46 +1,76 @@
 package com.example.ki.a10_25;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 
-import com.kakao.auth.AuthType;
+import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
 
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.security.MessageDigest;
 
 public class KakaoLoginActivity extends AppCompatActivity {
     private Context mContext;
-
+    private SessionCallback callback;
     private LoginButton btn_kakao_login;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kakao_login);
-        mContext = getApplicationContext();
 
-        getHashKey(mContext);
-        btn_kakao_login = (LoginButton) findViewById(R.id.com_kakao_login);
-        btn_kakao_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Session session = Session.getCurrentSession();
-
-                session.addCallback(new SessionCallBack());
-
-                session.open(AuthType.KAKAO_LOGIN_ALL, KakaoLoginActivity.this);
-            }
-        });
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
+        getHashKey(this);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
 
     // 프로젝트의 해시키를 반환
     @Nullable
@@ -68,4 +98,110 @@ public class KakaoLoginActivity extends AppCompatActivity {
             return null;
         }
     }
+    protected void redirectSignupActivity() {
+        final Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private String nickName;
+    private long id;
+
+    private class SessionCallback implements ISessionCallback {
+
+
+        @Override
+        public void onSessionOpened() {
+            requestMe();
+
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                Logger.e(exception);
+            }
+        }
+        // 사용자 정보 요청
+
+        public void requestMe() {
+
+            // 사용자정보 요청 결과에 대한 Callback
+
+            UserManagement.requestMe(new MeResponseCallback() {
+
+                // 세션 오픈 실패. 세션이 삭제된 경우,
+
+                @Override
+
+                public void onSessionClosed(ErrorResult errorResult) {
+
+                    Log.e("SessionCallback :: ", "onSessionClosed : " + errorResult.getErrorMessage());
+
+                }
+
+
+
+                // 회원이 아닌 경우,
+
+                @Override
+
+                public void onNotSignedUp() {
+
+                    Log.e("SessionCallback :: ", "onNotSignedUp");
+
+                }
+
+
+
+                // 사용자정보 요청에 성공한 경우,
+
+                @Override
+
+                public void onSuccess(UserProfile userProfile) {
+                    redirectSignupActivity();
+                    Log.e("SessionCallback :: ", "onSuccess");
+                    nickName = userProfile.getNickname();
+//                    String email = userProfile.getEmail();
+//                    String profileImagePath = userProfile.getProfileImagePath();
+//                    String thumnailPath = userProfile.getThumbnailImagePath();
+//                    String UUID = userProfile.getUUID();
+                    id = userProfile.getId();
+                    Log.e("Profile : ", nickName + "");
+                    Log.e("Profile id : ", id + "");
+                    redirectSignupActivity();
+                    makeJson();
+                }
+
+                // 사용자 정보 요청 실패
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+
+                    Log.e("SessionCallback :: ", "onFailure : " + errorResult.getErrorMessage());
+
+                }
+
+            });
+
+        }
+
+
+
+    }
+    protected JSONObject makeJson(){
+        JSONObject jsonObject = new JSONObject();
+        try{
+            jsonObject.put("nickname",nickName);
+            jsonObject.put("id",id);
+            Log.e("json : ",jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+
+
+
 }
+
